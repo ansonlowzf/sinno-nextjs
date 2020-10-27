1986e9e079d6c8f7b6e2c7c2610e564d2d7a58bd
Anson Low Z.F <zhanfaye@gmail.com>
2020-10-27 20:12:54 +0800

    change: list styles

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -10,6 +10,11 @@ const useStyles = makeStyles((theme) => ({
     listStyleType: `none`,
     marginBottom: theme.spacing(1),
   },
+  linkStyles: {
+    textDecoration: `none`,
+    color: theme.palette.primary.main,
+    fontWeight: 500,
+  },
 }));
 
 export default function Home({ allPostsData }) {
@@ -25,7 +30,7 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({ id, date, title }) => (
             <li className={classes.blogList} key={id}>
               <Link href={`/posts/${id}`}>
-                <a>{title}</a>
+                <a className={classes.linkStyles}>{title}</a>
               </Link>
               <br />
               <Typography
