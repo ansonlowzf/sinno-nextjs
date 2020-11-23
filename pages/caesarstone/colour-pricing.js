b48038ebc211b6b2af5eaad593d6e09e2b162fc4
Anson Low Z.F <zhanfaye@gmail.com>
2020-11-23 20:25:07 +0800

    page: caesarstone colour pricing

---
+++ pages/caesarstone/colour-pricing.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Heading2, SectionWrapper } from "../../elements";
+import { CaesarstoneColour } from "../../components";
+
+const ColourPricing = () => {
+  return (
+    <SectionWrapper>
+      <Heading2>Caesarstone Colour &amp; Pricing</Heading2>
+      <CaesarstoneColour />
+    </SectionWrapper>
+  );
+};
+
+export default ColourPricing;
