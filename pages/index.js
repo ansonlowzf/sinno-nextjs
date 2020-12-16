d8e637351cc11a0b6d61d8ce4cb7eae43f9ebcf3
Anson Low Z.F <zhanfaye@gmail.com>
2020-12-16 11:38:14 +0800

    start moving about to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { Container, Grid } from "@material-ui/core";
+import { Box, Container, Grid, Typography } from "@material-ui/core";
 import { Hero, BodyT1, FaqAccordion, StoneCard } from "../components";
-import { Heading2, SectionWrapper } from "../elements";
+import { Heading2, SectionWrapper, TextWrapper } from "../elements";
 import { NextSeo } from "next-seo";
 import { faqHomepage } from "../constants/faq";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -18,6 +19,27 @@ const Home = () => {
       />
 
       <SectionWrapper>
+        <Heading2>Stone Innovations Enterprise</Heading2>
+        <TextWrapper>
+          <Typography align="center" paragraph>
+            <Box fontWeight="fontWeightBold">Business Registration No.:</Box>{" "}
+            201603220786
+          </Typography>
+          <Typography align="center" paragraph>
+            <Box fontWeight="fontWeightBold">Sales Office:-</Box> 45-1-1, Jalan
+            Medan Putra 6, Medan Putra Business Center, Bandar Menjalara 52200
+            KL
+          </Typography>
+          <Typography align="center">
+            <Box fontWeight="fontWeightBold">
+              Factory &amp; Warehosue Address:-
+            </Box>{" "}
+            944E, Kampung Baru Sungai Buloh, 47000, Sungai Buloh, Selangor D.E
+          </Typography>
+        </TextWrapper>
+      </SectionWrapper>
+
+      <SectionWrapper>
         <BodyT1
           title="What We Do?"
           imgSrc="/empira_white.jpg"
@@ -28,6 +50,26 @@ const Home = () => {
           linkUrl="/about-us"
         />
       </SectionWrapper>
+
+      <Container maxWidth="md">
+        <Heading2>Warehouse</Heading2>
+        <TextWrapper>
+          <Typography align="center">
+            More than 800 slabs quartz stone, granite, sintered stone, marble
+            ready stock
+          </Typography>
+        </TextWrapper>
+        <Grid container justify="center">
+          <Grid item>
+            <Image
+              src="/aboutus/factory2.JPG"
+              alt="Factory"
+              width={800}
+              height={450}
+            />
+          </Grid>
+        </Grid>
+      </Container>
 
       <SectionWrapper>
         <Heading2 idPath="stones">Stone Product</Heading2>
