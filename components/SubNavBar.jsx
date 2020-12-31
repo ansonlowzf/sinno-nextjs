e0da47343e69e2014999f0a390de3f8712978194
Anson Low Z.F <zhanfaye@gmail.com>
2020-12-31 11:56:48 +0800

    create sub nav bar

--- components/SubNavBar.jsx
+++ components/SubNavBar.jsx
@@ -1,21 +1,45 @@
-import { makeStyles, AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  makeStyles,
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { ActiveLink } from "../components";
 
 const useStyles = makeStyles((theme) => ({
   toolbarContainer: {
     ...theme.mixins.toolbar,
+    display: `flex`,
+    justifyContent: `space-evenly`,
+  },
+  linkStyle: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: theme.palette.common.black,
+    opacity: 0.5,
+    "&.active": {
+      opacity: 1,
+    },
   },
 }));
 
-export const SubNavBar = () => {
+export const SubNavBar = ({ routes }) => {
   const classes = useStyles();
 
   return (
     <AppBar position="sticky" color="secondary">
-      <Toolbar className={classes.toolbarContainer}>
-        <Typography>Test 1</Typography>
-        <Typography>Test 2</Typography>
-        <Typography>Test 3</Typography>
-      </Toolbar>
+      <Container maxWidth="md">
+        <Toolbar className={classes.toolbarContainer}>
+          {routes.map(({ name, link }, index) => (
+            <Typography variant="button" key={`${name}${index}`}>
+              <ActiveLink activeClassName="active" href={link}>
+                <a className={classes.linkStyle}>{name}</a>
+              </ActiveLink>
+            </Typography>
+          ))}
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
