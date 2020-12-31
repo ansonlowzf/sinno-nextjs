8c8b676dbce43166c8a776dc1ab9cc6e59a50dca
Anson Low Z.F <zhanfaye@gmail.com>
2020-12-31 11:56:28 +0800

    remove hero scroll down

--- components/index.js
+++ components/index.js
@@ -11,7 +11,6 @@ export * from "./PriceTerm";
 export * from "./CaesarstoneColour";
 export * from "./ColourWarning";
 export * from "./CaesarstoneCommitment";
-export * from "./HeroScrollDown";
 export * from "./QuartzStoneFAB";
 export * from "./ActiveLink";
 export * from "./SideDrawer";
