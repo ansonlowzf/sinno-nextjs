46033ddd180b8ef49a792083bb8063c0fd8b1647
Anson Low Z.F <zhanfaye@gmail.com>
2020-12-31 11:55:06 +0800

    add routes constant

---
+++ constants/route.js
@@ -0,0 +1,10 @@
+export const qsRoutes = [
+  { name: "caesarstone", link: "/caesarstone" },
+  { name: "silestone", link: "/silestone" },
+  { name: "zenstone", link: "/zenstone-malaysia" },
+];
+
+export const ssRoutes = [
+  { name: "dekton", link: "/sintered-stone/dekton" },
+  { name: "mocastone", link: "/sintered-stone/mocastone" },
+];
