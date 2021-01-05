e0ee149aca3b41153524177087001c070a0fa087
Anson Low Z.F <zhanfaye@gmail.com>
2021-01-05 14:40:46 +0800

    add caesarstone pricing constant

---
+++ constants/pricing.js
@@ -0,0 +1,11 @@
+export const caesarstonePricing = {
+    standard = 'RM 400 /FR or RM 250 /SF',
+    deluxe = 'RM 410 /FR or RM 260 /SF',
+    premium = 'RM 500 /FR or RM 300 /SF',
+    supernatural = 'RM 600 /FR or RM 350 /SF',
+    supernaturalUltra = 'RM 1100 /FR or RM 650 /SF',
+    standardJumbo = 'RM 460 /FR or RM 300 /SF',
+    deluxeJumbo = 'RM 470 /FR or RM 310 /SF',
+    supernaturalJumbo = 'RM 700 /FR or RM 420 /SF',
+    supernaturalUltraJumbo = 'RM 1250 /FR or RM 750 /SF'
+}
