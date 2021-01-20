a38ecff63d687964eba354b4bb882de33c77a1a4
Anson Low Z.F <zhanfaye@gmail.com>
2021-01-20 09:52:55 +0800

    change blog layout css name to align the naming

--- components/BlogLayout.jsx
+++ components/BlogLayout.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Head from "next/head";
-import styles from "./layout.module.css";
+import styles from "./blogLayout.module.css";
 import Link from "next/link";
 import { makeStyles, Container, Typography, Button } from "@material-ui/core";
 
@@ -35,13 +35,7 @@ export function BlogLayout({ children, home }) {
     <Container maxWidth="sm" className={classes.containerStyles}>
       <Head>
         <link rel="icon" href="/favicon.png" />
-        <meta name="description" content="Stone surfacing related content" />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta name="description" content={siteTitle} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
