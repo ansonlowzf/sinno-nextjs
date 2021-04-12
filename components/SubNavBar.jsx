783eebe20401ae6fef3260fd329a58b2c7ee3511
Anson Low Z.F <zhanfaye@gmail.com>
2021-04-12 08:52:51 +0800

    change subnav style

--- components/SubNavBar.jsx
+++ components/SubNavBar.jsx
@@ -2,16 +2,16 @@ import {
   makeStyles,
   AppBar,
   Container,
+  Grid,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import { ActiveLink } from "../components";
 
 const useStyles = makeStyles((theme) => ({
-  toolbarContainer: {
-    ...theme.mixins.toolbar,
-    display: `flex`,
-    justifyContent: `space-evenly`,
+  gridStyle: {
+    paddingTop: "1.25rem",
+    paddingBottom: "1.25rem",
   },
   linkStyle: {
     textDecoration: `none`,
@@ -30,15 +30,59 @@ export const SubNavBar = ({ routes }) => {
   return (
     <AppBar position="sticky" color="secondary">
       <Container maxWidth="md">
-        <Toolbar className={classes.toolbarContainer}>
+        <Grid
+          container
+          justify="space-evenly"
+          alignItems="center"
+          spacing={2}
+          className={classes.gridStyle}
+        >
           {routes.map(({ name, link }, index) => (
-            <Typography variant="button" key={`${name}${index}`}>
+            <Grid
+              container
+              item
+              justify="space-evenly"
+              alignItems="center"
+              key={`${name}${index}`}
+              xs={12}
+              sm={4}
+            >
               <ActiveLink activeClassName="active" href={link}>
-                <a className={classes.linkStyle}>{name}</a>
+                <Typography
+                  component="a"
+                  variant="button"
+                  align="center"
+                  className={classes.linkStyle}
+                >
+                  {name}
+                </Typography>
               </ActiveLink>
-            </Typography>
+            </Grid>
           ))}
-        </Toolbar>
+        </Grid>
+        {/* <Toolbar className={classes.toolbarContainer}>
+          {routes.map(({ name, link }, index) => (
+            <Grid
+              container
+              justify="space-evenly"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item key={`${name}${index}`} xs={12}>
+                <ActiveLink activeClassName="active" href={link}>
+                  <Typography
+                    component="a"
+                    variant="button"
+                    align="center"
+                    className={classes.linkStyle}
+                  >
+                    {name}
+                  </Typography>
+                </ActiveLink>
+              </Grid>
+            </Grid>
+          ))}
+        </Toolbar> */}
       </Container>
     </AppBar>
   );
